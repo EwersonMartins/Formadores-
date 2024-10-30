42539ba13dc60e63a36cd180ecfbbff505b51b12
EwersonMartins <141281003+EwersonMartins@users.noreply.github.com>
2024-10-30 15:33:51 -0300

    app.js

---
+++ perguntas.js
@@ -0,0 +1,16 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Pyton é uma linguagem de programção',
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da Fança',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'programação',
+    'O que é uma função?',
+    'uma função é um bloco de cédigo que executa uma tarefa'
+)
+criaCartao
